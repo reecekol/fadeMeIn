648ec820e1d794644738a1416a1b84009f044945
reecek <reecekol@gmail.com>
2013-11-09 20:37:16 -0500

    init

---
+++ fadeMeIn.js
@@ -0,0 +1,11 @@
+/*    fadeMeIn.js 
+ *    version v1.0 (11/2013)
+ *    Copyright 2013 Kole Reece.
+ *    Released under the MIT license.
+ *    Dependencies: jQuery v1.4+ 
+ */
+
+(function($){
+
+
+})(jQuery);
